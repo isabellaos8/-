8468fda669de344bf23fb312242e3b0f4dd35694
Isabella Oliva Solis <iolivasolis@ggc.edu>
2024-01-16 16:54:11 -0500

    Initial commit

---
+++ src/Banana.java
@@ -0,0 +1,14 @@
+/**
+*class: MyFirstProgram
+ * @Author: Isabella Oliva
+ * @Date: 1/11/24
+ * * @version: 1.0
+ * @course name: ITEC 2140 sec. 3 Spring 2024
+ * description: this class will print out the message about Banana
+*/
+
+public class Banana {
+    public state void main(String[] args){
+        System.out.print
+    }
+}
---
+++ src/Fibonacci.java
@@ -0,0 +1,11 @@
+/**Class: Fibonacci
+ * Author: Isabella Oliva Solis
+ * Version: 1.0
+ * Course:ITEC 2140 section 03 Spring 2024
+ * Written: January 16, 2024
+ *
+ * This class will print out the sequence fibonacci number
+ */
+
+public class Fibonachi {
+}
